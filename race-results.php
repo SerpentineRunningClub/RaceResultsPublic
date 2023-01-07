fcf14902b35442406502bcd36a38ea7fe9eaf025
Philip Whitehouse <philip@whiuk.com>
2023-01-07 19:40:11 +0000

    Adding initial module definition file

---
+++ race-results.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Race Results
+* Plugin URI: https://www.serpentine.org.uk/
+* Description: The Serpentine Race Results Plugin
+* Version: 1.0
+* Author: Serpentine Running Club
+* Author URI: http://www.serpentine.org.uk/
+* Text Domain: race-results
+**/
